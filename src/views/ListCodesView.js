d1962e5573c6f46f26a7a9d9af96905ca9d2eedf
zhcoder <zhcoder@yandex.ru>
2021-11-05 02:51:52 +1000

    updates

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -6,19 +6,17 @@ bridge.subscribe((e) => bridgeEvent(e));
 
 let CodesContext = React.createContext('test');
 
-
 function bridgeEvent(e) {
     if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
-        const arr = e.detail.data.keys;
-        CodesContext = React.createContext(arr);
+        // const arr = e.detail.data.keys;
+        CodesContext = React.createContext("123");
     }
 }
 
 function QRCodeView() {
-
+    bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
     console.log("Список кодов:", React.useContext(CodesContext));
     // const [activeView, setActiveView] = useState('f');
-    // bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
     return (
         <View activePanel="main" id="view2">
             <Panel id="main">
