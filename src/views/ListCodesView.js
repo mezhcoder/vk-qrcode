34492008cc4537eed0b8349b07cfb40da688832a
zhcoder <zhcoder@yandex.ru>
2021-11-05 03:11:18 +1000

    update

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -3,20 +3,23 @@ import { Group, Panel, View} from "@vkontakte/vkui";
 import bridge from "@vkontakte/vk-bridge";
 
 
+let global_arr = [];
+
+bridge.subscribe((e) => bridgeEvent(e));
+function bridgeEvent(e) {
+    if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
+        const arr = e.detail.data.keys;
+        global_arr = arr;
+        console.log("отправлен запрос +1");
+    }
+}
+bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
+
+
 
 function QRCodeView() {
     const [codes, setCodes] = useState([]);
-
-    function bridgeEvent(e) {
-        if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
-            const arr = e.detail.data.keys;
-            setCodes(arr);
-            console.log("отправлен запрос +1");
-        }
-    }
-
-    bridge.subscribe((e) => bridgeEvent(e));
-    bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
+    setCodes(global_arr);
 
     return (
         <View activePanel="main" id="view2">
