aa35885f40c3a484b72de1a824bed005d3eaf874
zhcoder <zhcoder@yandex.ru>
2021-11-05 04:15:52 +1000

    update

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -11,21 +11,22 @@ import {useAsync} from 'react-async';
 
 const myFunction = () => {
     return new Promise((resolve, reject) => {
+
         bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
         bridge.subscribe((e) => bridgeEvent(e));
         function bridgeEvent(e) {
             if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
                 // setCodes(e.detail.data.keys);
                 console.log("запрос +1");
+                bridge.unsubscribe(e);
+                bridge.unsubscribe(bridgeEvent);
                 bridge.unsubscribe(this);
                 resolve(e.detail.data.keys);
             }
         }
     });
 };
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    //     .then(res => (res.ok ? res : Promise.reject(res)))
-    //     .then(res => res.json())
+
 
 function QRCodeView() {
 
