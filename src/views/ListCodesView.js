f770d9ecfcf830b525e62feb762910d2bb6b7230
zhcoder <zhcoder@yandex.ru>
2021-11-05 03:03:32 +1000

    update

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -1,27 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { Group, Panel, View} from "@vkontakte/vkui";
 import bridge from "@vkontakte/vk-bridge";
 
-bridge.subscribe((e) => bridgeEvent(e));
 
-let CodesContext = React.createContext('test');
-
-function bridgeEvent(e) {
-    if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
-        const arr = e.detail.data.keys;
-        CodesContext = React.createContext(arr);
-    }
-}
 
 function QRCodeView() {
+    const [codes, setCodes] = useState([]);
+
+    function bridgeEvent(e) {
+        if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
+            const arr = e.detail.data.keys;
+            setCodes(arr);
+        }
+    }
+
+    bridge.subscribe((e) => bridgeEvent(e));
     bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
-    console.log("Список кодов:", React.useContext(CodesContext));
-    // const [activeView, setActiveView] = useState('f');
+
     return (
         <View activePanel="main" id="view2">
             <Panel id="main">
                 <Group>
                     <h1>Список твоих QR кодов</h1>
+                    {"Arr"} {codes.toString()}
                 </Group>
             </Panel>
         </View>
