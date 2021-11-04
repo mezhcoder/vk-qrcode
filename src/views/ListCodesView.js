c6a30668698623f38ee9b064e42c0f75a5bbcc09
zhcoder <zhcoder@yandex.ru>
2021-11-05 04:05:54 +1000

    update

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -1,30 +1,43 @@
-import React, {useEffect, useState} from "react";
+import React  from "react";
 import {Group, Panel, View} from "@vkontakte/vkui";
 import bridge from "@vkontakte/vk-bridge";
+import {useAsync} from 'react-async';
 
+
+
+
+
+
+
+const myFunction = () => {
+    return new Promise((resolve, reject) => {
+        bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
+        bridge.subscribe((e) => bridgeEvent(e));
+        function bridgeEvent(e) {
+            if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
+                // setCodes(e.detail.data.keys);
+                console.log("запрос +1");
+                resolve(e.detail.data.keys);
+            }
+        }
+
+    });
+};
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then(res => (res.ok ? res : Promise.reject(res)))
+    //     .then(res => res.json())
 
 function QRCodeView() {
-    const [codes, setCodes] = useState([]);
 
-    bridge.subscribe((e) => bridgeEvent(e));
-    function bridgeEvent(e) {
-        if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
-            setCodes(e.detail.data.keys);
-            console.log("отправлен запрос +1");
-        }
-    }
-
-    useEffect(() => {
-        console.log("render");
-        bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
-    }, [])
+    const { data, error, isLoading } = useAsync({ promiseFn: myFunction });
+    if (isLoading) return "Loading...";
+    if (error) return `Something went wrong: ${error.message}`
 
     return (
         <View activePanel="main" id="view2">
             <Panel id="main">
                 <Group>
-                    <h1>Список твоих QR кодов</h1>
-                    {"Arr"} {codes.toString()}
+                    Data: {data}
                 </Group>
             </Panel>
         </View>
