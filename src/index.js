3bababecad393c0fee3c15f576d60ef4d4964904
zhcoder <zhcoder@yandex.ru>
2021-11-04 20:14:34 +1000

    init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+import "./css/main.css";
+
+import "@vkontakte/vkui/dist/vkui.css";
+import bridge from '@vkontakte/vk-bridge';
+bridge.send("VKWebAppInit", {}).then(r => console.log("Bridge:" , r));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
