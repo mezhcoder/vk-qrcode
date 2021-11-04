c7c3aa46972dc5b0aa2c38b3cc2c7b9821340868
zhcoder <zhcoder@yandex.ru>
2021-11-05 03:20:47 +1000

    update

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -1,25 +1,23 @@
-import React, {useState} from "react";
-import { Group, Panel, View} from "@vkontakte/vkui";
+import React, {useEffect, useState} from "react";
+import {Group, Panel, View} from "@vkontakte/vkui";
 import bridge from "@vkontakte/vk-bridge";
-
-
-let global_arr = [];
-
-bridge.subscribe((e) => bridgeEvent(e));
-function bridgeEvent(e) {
-    if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
-        const arr = e.detail.data.keys;
-        global_arr = arr;
-        console.log("отправлен запрос +1");
-    }
-}
-bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
-
 
 
 function QRCodeView() {
     const [codes, setCodes] = useState([]);
-    setCodes(global_arr);
+
+    bridge.subscribe((e) => bridgeEvent(e));
+    function bridgeEvent(e) {
+        if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
+            setCodes(e.detail.data.keys);
+            console.log("отправлен запрос +1");
+        }
+    }
+
+    useEffect(() => {
+        console.log("render");
+        bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
+    }, [])
 
     return (
         <View activePanel="main" id="view2">
