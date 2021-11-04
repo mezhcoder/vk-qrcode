fb58fec2fb1cab81653654131e44424c8cf7b755
zhcoder <zhcoder@yandex.ru>
2021-11-05 01:14:44 +1000

    update

--- src/views/ListCodesView.js
+++ src/views/ListCodesView.js
@@ -2,12 +2,16 @@ import React from "react";
 import {Div, Button, Group, Panel, PanelHeader, View, CardGrid, Card} from "@vkontakte/vkui";
 import bridge from "@vkontakte/vk-bridge";
 
+bridge.subscribe((e) => bridgeEvent(e));
 
-function send() {
-    bridge.send("VKWebAppOpenCodeReader");
+function bridgeEvent(e) {
+    if (e.detail.type === 'VKWebAppStorageGetKeysResult') {
+        console.log("Переменные:", e.detail.data.keys);
+    }
 }
 
 function QRCodeView() {
+    bridge.send("VKWebAppStorageGetKeys", {"count": 20, "offset": 0});
     return (
         <View activePanel="main" id="view2">
             <Panel id="main">
